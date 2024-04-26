8f1b57f12aa248ec1a0756d61855e5638c3b8688
shyam <shyamlal@gmail.com>
2024-04-26 06:45:11 +0530

    Update entity

--- com.tunehub/src/main/java/com/example/main/entity/Users.java
+++ com.tunehub/src/main/java/com/example/main/entity/Users.java
@@ -16,11 +16,16 @@ public class Users {
 	String gender;
 	String role;
 	String address;
+	
+	boolean isPremium;
+	
 	public Users() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	public Users(int id, String username, String email, String password, String gender, String role, String address) {
+
+	public Users(int id, String username, String email, String password, String gender, String role, String address,
+			boolean isPremium) {
 		super();
 		this.id = id;
 		this.username = username;
@@ -29,52 +34,78 @@ public class Users {
 		this.gender = gender;
 		this.role = role;
 		this.address = address;
+		this.isPremium = isPremium;
 	}
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getUsername() {
 		return username;
 	}
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public String getGender() {
 		return gender;
 	}
+
 	public void setGender(String gender) {
 		this.gender = gender;
 	}
+
 	public String getRole() {
 		return role;
 	}
+
 	public void setRole(String role) {
 		this.role = role;
 	}
+
 	public String getAddress() {
 		return address;
 	}
+
 	public void setAddress(String address) {
 		this.address = address;
 	}
+
+	public boolean isPremium() {
+		return isPremium;
+	}
+
+	public void setPremium(boolean isPremium) {
+		this.isPremium = isPremium;
+	}
+
 	@Override
 	public String toString() {
 		return "Users [id=" + id + ", username=" + username + ", email=" + email + ", password=" + password
-				+ ", gender=" + gender + ", role=" + role + ", address=" + address + "]";
+				+ ", gender=" + gender + ", role=" + role + ", address=" + address + ", isPremium=" + isPremium + "]";
 	}
+	
+	
 }

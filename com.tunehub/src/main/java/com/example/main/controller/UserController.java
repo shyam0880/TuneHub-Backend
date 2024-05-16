0e6eab86c2b9ed0c1e827f3fb5e0c56e27be50d6
shyam <shyamlal@gmail.com>
2024-05-16 13:54:18 +0530

    if premium display table

--- com.tunehub/src/main/java/com/example/main/controller/UserController.java
+++ com.tunehub/src/main/java/com/example/main/controller/UserController.java
@@ -1,12 +1,17 @@
 package com.example.main.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.main.entity.Song;
 import com.example.main.entity.Users;
+import com.example.main.services.SongService;
 import com.example.main.services.UsersService;
 
 import jakarta.servlet.http.HttpSession;
@@ -18,6 +23,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class UserController {
 	@Autowired
 	UsersService srv;
+	
+	@Autowired
+	SongService songService;
 	
 	@PostMapping("/register")
 	public String addUsers(@ModelAttribute Users user)
@@ -38,7 +46,7 @@ public class UserController {
 	
 	@PostMapping("/validate")
 	//public String validate(@RequestParam("email") String email,@RequestParam("password") String password) we can use this also
-	public String validate(@RequestParam String email,String password,HttpSession session)
+	public String validate(@RequestParam String email,String password,HttpSession session, Model model)
 	{
 		if(srv.validateUser(email,password)== true)
 		{
@@ -53,6 +61,13 @@ public class UserController {
 			}
 			else
 			{
+				Users user = srv.getUser(email);
+				boolean userStatus = user.isPremium();
+				model.addAttribute("isPremium", userStatus);
+				
+				//for song view
+				List <Song> songList = songService.fetchAllSongs();
+				model.addAttribute("songs", songList);
 				return "customerhome";				
 			}
 		}
